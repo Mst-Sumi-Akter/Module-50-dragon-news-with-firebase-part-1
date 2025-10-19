393d1ec296eed72454040ea46ddb9bb13ca04118
Sumi Akter <sumiwdcse@gmail.com>
2025-10-19 23:35:40 +0600

    gate ready to build  dragon news

---
+++ src/routes/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+const router = createBrowserRouter(
+    [
+        {
+           path:"/",
+           element: <HomeLayout></HomeLayout>
+        },
+        {
+           path:"/auth",
+           element: <h1>Authentication Layout</h1>
+        },
+        {
+           path:"/news",
+           element: <h1>news Layout</h1>
+        },
+    ]
+);
+export default router;
