5953284b437b808e88e447fb28790c50a4dd514f
Sumi Akter <sumiwdcse@gmail.com>
2025-10-20 02:40:11 +0600

    gate ready to build  dragon news pactice 4

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../components/Header';
+import LatestNews from '../components/LatestNews';
+import Navbar from '../components/Navbar';
+import LeftAside from '../components/homelayout/LeftAside';
+import RightAside from '../components/homelayout/RightAside';
+
 
 const HomeLayout = () => {
     return (
         <div>
             <header>
                 <Header></Header>
+                <section className='w-11/12 mx-auto my-3'>
+                    <LatestNews></LatestNews>
+                </section>
+                <nav className='w-11/12 mx-auto my-3'>
+                    <Navbar></Navbar>
+                </nav>
             </header>
-            <main>
-                <section className='left_nav'></section>
+            <main className=''>
+                <aside>
+                    <LeftAside></LeftAside>
+                </aside>
                 <section className='main'> <Outlet></Outlet> </section>
+                <aside>
+                 <RightAside></RightAside>
+                </aside>
                 <section className='right_nav'></section>
             </main>
         </div>
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,23 @@
 import { createBrowserRouter } from "react-router";
 import HomeLayout from "../layouts/HomeLayout";
+import CategoryNews from "../pages/CategoryNews";
+import Home from "../pages/Home";
 const router = createBrowserRouter(
     [
         {
            path:"/",
-           element: <HomeLayout></HomeLayout>
+           element: <HomeLayout></HomeLayout>,
+            children: [
+      {
+        path: "",
+        element: <Home></Home>,
+      },
+      {
+        path: "/category/:id",
+        element: <CategoryNews></CategoryNews>,
+      //   loader: () => fetch("/news.json"),
+      },
+    ],
         },
         {
            path:"/auth",
